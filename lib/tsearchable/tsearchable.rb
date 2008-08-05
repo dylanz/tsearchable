348851e89136c8faec2d32a510a8cadfe11b08c1
Florent <florent@Niobe.local>
2008-08-06 00:17:33 +0200

    added trgm creation and find methods

--- lib/tsearchable/tsearchable.rb
+++ lib/tsearchable/tsearchable.rb
@@ -6,12 +6,13 @@ module TSearchable
 
   module ClassMethods
     def tsearchable(options = {})
-      @config = {:index => 'gin', :vector_name => 'ts_index'}
+      @config = {:index => 'gin', :vector_name => 'ts_index', :suggest => [] }
       @config.update(options) if options.is_a?(Hash)
       @config.each {|k,v| instance_variable_set(:"@#{k}", v)}
       raise "You must explicitly specify which fields you want to be searchable" unless @fields
 
       @indexable_fields = @fields.inject([]) {|a,f| a << "coalesce(#{f.to_s},'')"}.join(' || \' \' || ')
+      @suggestable_fields = options[:suggest]
 
       after_save :update_tsvector_row
       define_method(:per_page) { 30 } unless respond_to?(:per_page)
@@ -60,6 +61,25 @@ module TSearchable
       results = find_by_text_search(keyword, options)
       results.empty? ? 0 : results.at(0)[:count].to_i
     end
+    
+    # TODO: implementer la pagination
+    def find_by_trgm(keyword, options = {})
+      raise ActiveRecord::RecordNotFound, "Couldn't find #{name} without a keyword" if keyword.blank?
+      return if @suggestable_fields.empty?
+
+      query = []
+      sel = []
+      @suggestable_fields.each do |field|
+        query  << "#{field} % '#{clean(keyword)}'"
+        sel << "similarity(#{field}, '#{clean(keyword)}') AS sml_#{field}"
+      end
+      query = query.join(" AND ")
+      sel = sel.join(", ")
+      options[:conditions] ? (options[:conditions] << ("AND " << query)) : (options[:conditions] = query)
+      options[:select] = "*, " << sel
+      
+      find(:all, options)
+    end
 
     def update_tsvector(rowid = nil)
       create_tsvector unless column_names.include?(@vector_name)
@@ -76,11 +96,22 @@ module TSearchable
     # creates the tsvector column and the index
     def create_tsvector(sql = [])
       return if column_names.include?(@vector_name)
+      
       sql << "ALTER TABLE #{table_name} ADD COLUMN #{@vector_name} tsvector"
       sql << "CREATE INDEX #{table_name}_ts_idx ON #{table_name} USING #{@index}(#{@vector_name})"
       sql.each {|s| update_table { connection.execute(s) }}
     end
     alias_method :create_vector, :create_tsvector
+    
+    # creates the trigram index
+    def create_trgm(sql = [])
+      return if column_names.include?(@vector_name)
+      
+      @suggestable_fields.each do |field|
+        sql << "CREATE INDEX index_#{table_name}_#{table_name}_trgm ON #{table_name} USING gist(#{field} gist_trgm_ops)"
+      end
+      sql.each {|s| update_table { connection.execute(s) }}
+    end
 
     # googly search terms to tsearch format.  jacked from bens acts_as_tsearch.
     def parse(query)
@@ -92,6 +123,10 @@ module TSearchable
       end.flatten!
       query.shift
       query.join
+    end
+    
+    def clean(query)
+      query
     end
 
     # always reset the column info !
